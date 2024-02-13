137dcd9bedea1ac50eced087e40fa6b0913b7795
mitas <mitage1090@gmail.com>
2024-02-13 16:59:48 +0900

    クライアントサイドの初期追加

---
+++ VueSearchPatent/src/components/DemoHeader.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="header">
+    <h1>{{ msg }}</h1>
+    <p>--LLMを使ったキーワード抽出とキーワード一致数による検索--</p>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  text-align: center; /* テキストを中央揃えに */
+  padding: 20px;
+  background-color: #333; /* ヘッダーの背景色 */
+  color: #fff; /* ヘッダーの文字色 */
+}
+.green {
+  color: #4CAF50; /* タイトルの色 */
+}
+</style>
+
