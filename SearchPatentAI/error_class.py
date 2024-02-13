94a6f3b3d23d33eb886cac5640d49c320fa124a4
Mitamura Takuma <83048191+mitas30@users.noreply.github.com>
2024-02-14 00:29:36 +0900

    Delete SearchPatentAI directory

--- SearchPatentAI/error_class.py
+++
@@ -1,3 +0,0 @@
-class SplitError(Exception):
-    """特定のキーワードで分割できなかった場合の例外。"""
-    pass
--- SearchPatentAI/search_result/cache/joblib/sklearn/cluster/_agglomerative/_average_linkage/func_code.py
+++
@@ -1,4 +0,0 @@
-# first line: 709
-def _average_linkage(*args, **kwargs):
-    kwargs["linkage"] = "average"
-    return linkage_tree(*args, **kwargs)
